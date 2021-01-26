806e5f73b9d3d93703cf4baf7dde5008f8d1a07f
muminv <muminv@tabgida.com.tr>
2021-01-26 12:27:41 +0300

    icons and logos

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -23,8 +23,8 @@ const Logo = ({
           <Image
             src={require('./../../../assets/images/logo.svg')}
             alt="Open"
-            width={32}
-            height={32} />
+            width={175}
+            />
         </Link>
       </h1>
     </div>
